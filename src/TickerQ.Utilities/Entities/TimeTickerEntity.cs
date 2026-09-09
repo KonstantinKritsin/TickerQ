6a3a9e0045a5135fb73ee50d7b4be48e2f3389e2
Konstantin Kritsin <konstantin.kritsin@broit.io>
2026-09-09 12:53:30 +0300

    Expose persistence materialization setters

--- src/TickerQ.Utilities/Entities/TimeTickerEntity.cs
+++ src/TickerQ.Utilities/Entities/TimeTickerEntity.cs
@@ -12,27 +12,27 @@ namespace TickerQ.Utilities.Entities
     public class TimeTickerEntity<TTicker> : BaseTickerEntity where TTicker : TimeTickerEntity<TTicker>
     {
         [JsonInclude]
-        public virtual TickerStatus Status { get; protected internal set; }
+        public virtual TickerStatus Status { get; set; }
         [JsonInclude]
-        public virtual string LockHolder { get; protected internal set; }
+        public virtual string LockHolder { get; set; }
         public virtual byte[] Request { get; set; }
         public virtual DateTime? ExecutionTime { get; set; }
         [JsonInclude]
-        public virtual DateTime? LockedAt { get; protected internal set; }
+        public virtual DateTime? LockedAt { get; set; }
         [JsonInclude]
-        public virtual DateTime? ExecutedAt { get; protected internal set; }
+        public virtual DateTime? ExecutedAt { get; set; }
         [JsonInclude]
-        public virtual string ExceptionMessage { get; protected internal set; }
+        public virtual string ExceptionMessage { get; set; }
         [JsonInclude]
-        public virtual string SkippedReason { get; protected internal set; }
+        public virtual string SkippedReason { get; set; }
         [JsonInclude]
-        public virtual long ElapsedTime { get; protected internal set; }
+        public virtual long ElapsedTime { get; set; }
         public virtual int Retries { get; set; }
         [JsonInclude]
-        public virtual int RetryCount { get; protected internal set; }
+        public virtual int RetryCount { get; set; }
         public virtual int[] RetryIntervals { get; set; }
         [JsonInclude]
-        public virtual Guid? ParentId { get; protected internal set; }
+        public virtual Guid? ParentId { get; set; }
         [JsonIgnore]
         public virtual TTicker Parent { get; protected internal set; }
         public virtual ICollection<TTicker> Children { get; set; } = new List<TTicker>();
